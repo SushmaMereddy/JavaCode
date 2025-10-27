cb94b7711a47d90676596a274083e88f648d8092
SushmaMereddy <sushmamr231@gmail.com>
2025-10-27 18:43:22 -0500

    variables demo

---
+++ VariableDemo.java
@@ -0,0 +1,15 @@
+class VariableDemo {
+	public static void main(String[] args){
+	int a=10;
+	double b=5.5;
+	char c='q';
+	boolean d=true;
+	System.out.println("Initialization for Integer: " +a);
+	System.out.println("Double: " +b);
+	System.out.println("Character: " +c);
+	System.out.println("Boolean: " +d);
+	
+	a=20;
+	System.out.println("Assignment: " +a);
+	}
+}
