6567af2d096123ef2091439b703d487862d3310b
SushmaMereddy <sushmamr231@gmail.com>
2025-10-27 19:15:27 -0500

    conditional operator demo code

---
+++ ConditionalOperatorDemo.java
@@ -0,0 +1,10 @@
+class ConditionalOperatorDemo {
+	public static void main(String[] args) {
+	String res = 5<6 ? "Hi" : "Bye";
+	System.out.println(res);
+	
+	int a=4;
+	String res1= (a%2==0) ? "even" : "odd";
+	System.out.println(res1);
+	}
+}
