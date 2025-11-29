81d9e1f534c9df6308969ea92928dcb2fe5e10ad
SushmaMereddy <sushmamr231@gmail.com>
2025-11-29 01:00:16 -0600

    VariablesDemo.java

---
+++ VariablesDemo.java
@@ -0,0 +1,19 @@
+class VariablesDemo {
+	public static void main(String[] args) {
+		int a=4; // intialization
+		System.out.println(a);
+		
+		a=5; //Assignment
+		System.out.println(a);
+		
+		int i=10;
+		double d=5.5;
+		char c='S';
+		boolean b=true;
+		
+		System.out.println("Integer: " +i);
+		System.out.println("Double: " +d);
+		System.out.println("Character: " +c);
+		System.out.println("Boolean: " +b);	
+	}
+}
